a1cfe53fefa92a617024bd6c48c51428e507b0ad
Febin John James <jamesfebin@gmail.com>
2016-08-19 22:21:44 +0530

    Ajax added

--- NewsReaderWeb/app.py
+++ NewsReaderWeb/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,jsonify
+from flask import Flask,jsonify,render_template
 from database import init_db
 from sqlalchemy.ext.serializer import loads, dumps
 from models import News
@@ -17,26 +17,9 @@ class NewsSchema(ma.Schema):
 news_item_schema = NewsSchema()
 news_schema = NewsSchema(many=True)
 
-
 @app.route("/")
 def home():
-    tech_news = News.query.filter(News.category=='tech').order_by(News.published.desc()).limit(2).all()
-    entertainment_news = News.query.filter(News.category=='business').order_by(News.published.desc()).limit(2).all()
-    business_news = News.query.filter(News.category=='entertainment').order_by(News.published.desc()).limit(2).all()
-    news = tech_news + entertainment_news + business_news
-    html = ''
-    old_category = ''
-    index = 0
-    for news_item in news:
-        html = html + "<center>"
-        if old_category != news_item.category:
-            html = html + "<h1>" + news_item.category.capitalize() +"</h1>"
-            old_category = news_item.category
-        if news_item.image != '':
-            html = html + "<img width=70% src='"+news_item.image+"'/>"
-        html = html + " <a style='text-decoration:none;' href='"+ news_item.link + "'>"+ "<p style='color:blue' >" + news_item.title+ "</p></a>"
-        html = html + "</center>"
-    return html
+    return render_template('index.html')
 
 @app.route("/api/news")
 def news():
@@ -44,7 +27,7 @@ def news():
     entertainment_news = News.query.filter(News.category=='business').order_by(News.published.desc()).limit(2).all()
     business_news = News.query.filter(News.category=='entertainment').order_by(News.published.desc()).limit(2).all()
     news = tech_news + entertainment_news + business_news
-    news = news_schema.dump(news)
-    return json.dumps(news)
+    return news_schema.jsonify(news)
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=3000, debug=True)
